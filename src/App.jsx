374fde204a3ade0e925de027d07ba47212e4898e
Adrian Roger <adrianroge@gmail.com>
2024-08-22 18:32:58 -0300

    feat: update project and components router

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,44 @@
 import {
+  Header,
   MainContainer,
   HeroSection,
   StaffSection,
   Carousel,
+  Card,
+  BackingSection,
+  Location,
+  Completion,
+  TimelineSection,
+
+  Footer,
 } from "./components";
 
 
+const backingData = {
+  name: "DEPUTADO JÚLIO CESAR",
+  image: "images/DEPUTADO_JULIO_CESAR.png",
+};
+
 function App() {
+  
   return (
     <>
+      <Header />
       <MainContainer>
         <HeroSection />
+        <StaffSection>
+          <Carousel />
+        </StaffSection>
+        <TimelineSection />
+        <Location />
+        <BackingSection>
+          <Card {...backingData} />
+        </BackingSection>
+        <Completion />
       </MainContainer>
-      <StaffSection>
-        <Carousel />
-      </StaffSection>
+      <Footer />
     </>
   );
 }
-// TODO: Criar o card do carousel e estilizar
+
 export default App;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,12 +4,26 @@ import ToggleThemeButton from "./ToggleThemeButton";
 import StaffSection from "./StaffSection";
 import Carousel from "./Carousel";
 import Card from "./Card";
+import BackingSection from "./BackingSection";
+import Location from "./Location";
+import Completion from "./Completion";
+import Header from "./Header";
+import Footer from "./Footer";
+import SubscribeModal from "./SubcribeModal";
+import TimelineSection from "./TimelineSction";
 
 export { 
+    Header,
     MainContainer, 
     HeroSection, 
     ToggleThemeButton, 
     StaffSection,
     Carousel,
     Card,
+    BackingSection,
+    Location,
+    Completion,
+    Footer,
+    SubscribeModal,
+    TimelineSection,
 };
